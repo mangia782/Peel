3d3c74a4cc4a9a7605c6920a8382a161fbe0568a
mangia782 <75375564+mangia782@users.noreply.github.com>
2024-04-29 00:31:57 +0000

    color and logo and loading icon

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -274,7 +274,7 @@ const RentModal = () => {
     <Modal
       disabled={isLoading}
       isOpen={rentModal.isOpen}
-      title="Airbnb your home!"
+      title="Upload a Fruit Find!"
       actionLabel={actionLabel}
       onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={secondaryActionLabel}
