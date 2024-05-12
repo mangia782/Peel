ec1a3433002b9674e5a5aa03ef36aaf40e4ba14d
mangia782 <75375564+mangia782@users.noreply.github.com>
2024-05-12 04:28:49 +0000

    modified:   app/components/inputs/ImageUpload.tsx 	modified:   app/components/modals/SearchModal.tsx 	modified:   app/components/navbar/Search.tsx fixed image upload and updated search function

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -14,41 +14,14 @@ const Search = () => {
   const { getByValue } = useCountries();
 
   const  locationValue = params?.get('locationValue'); 
-  const  startDate = params?.get('startDate');
-  const  endDate = params?.get('endDate');
-  const  guestCount = params?.get('guestCount');
 
   const locationLabel = useMemo(() => {
     if (locationValue) {
       return getByValue(locationValue as string)?.label;
     }
 
-    return 'Any Store';
+    return 'search for a store or fruit';
   }, [locationValue, getByValue]);
-
-  const durationLabel = useMemo(() => {
-    if (startDate && endDate) {
-      const start = new Date(startDate as string);
-      const end = new Date(endDate as string);
-      let diff = differenceInDays(end, start);
-
-      if (diff === 0) {
-        diff = 1;
-      }
-
-      return `${diff} Days`;
-    }
-
-    return 'Any Fruit'
-  }, [startDate, endDate]);
-
-  const guestLabel = useMemo(() => {
-    if (guestCount) {
-      return `${guestCount} Guests`;
-    }
-
-    return 'Add To Cart';
-  }, [guestCount]);
 
   return ( 
     <div
@@ -68,7 +41,6 @@ const Search = () => {
       <div 
         className="
           flex 
-          flex-row 
           items-center 
           justify-between
         "
@@ -76,39 +48,27 @@ const Search = () => {
         <div 
           className="
             text-sm 
+            text-gray-600 
             font-semibold 
             px-6
+            items-center 
           "
         >
           {locationLabel}
-        </div>
-        <div 
-          className="
-            hidden 
-            sm:block 
-            text-sm 
-            font-semibold 
-            px-6 
-            border-x-[1px] 
-            flex-1 
-            text-center
-          "
-        >
-          {durationLabel}
+          
         </div>
         <div 
           className="
             text-sm 
             pl-6 
             pr-2 
-            text-gray-600 
+            text-gray-400 
             flex 
             flex-row 
             items-center 
             gap-3
           "
         >
-          <div className="hidden sm:block">{guestLabel}</div>
           <div 
             className="
               p-2 
