bdfcafa1df3085efef7e9d7b323b2d3e81220567
mangia782 <75375564+mangia782@users.noreply.github.com>
2024-05-14 04:26:16 +0000

    modified:   app/components/listings/ListingInfo.tsx 	modified:   app/listings/[listingId]/ListingClient.tsx fixed extra variable errors (?)

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -16,9 +16,6 @@ const Map = dynamic(() => import('../Map'), {
 interface ListingInfoProps {
   user: SafeUser,
   description: string;
-  guestCount: number;
-  roomCount: number;
-  bathroomCount: number;
   category: {
     icon: IconType,
     label: string;
@@ -31,9 +28,6 @@ interface ListingInfoProps {
 const ListingInfo: React.FC<ListingInfoProps> = ({
   user,
   description,
-  guestCount,
-  roomCount,
-  bathroomCount,
   category,
   locationValue,
   locationAddress,
